120d10cf9bb9aa682275c78fb718b0675fb49139
Irvin <reimer1693@gmail.com>
2022-10-13 11:34:57 -0500

    añadido variables de entorno

--- server/db.js
+++ server/db.js
@@ -1,11 +1,12 @@
 import { createPool } from "mysql2/promise"
+import { DB_HOST, DB_NAME, DB_PASSWORD, DB_USER, DB_PORT } from "./config.js" 
 
 export const pool =  createPool({
-    host:'radionortechico.com',
-    port:3306,
-    user:'wlqgsapm_reimer',
-    password:'kovenant16',
-    database:'wlqgsapm_prueba'
+    host:DB_HOST,
+    port:DB_PORT,
+    user:DB_USER,
+    password:DB_PASSWORD,
+    database:DB_NAME
 })
 
 
