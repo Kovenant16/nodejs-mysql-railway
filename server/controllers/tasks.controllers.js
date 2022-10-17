1699e0230598f6d518eb3062747d1350f2e55dd6
Irvin <reimer1693@gmail.com>
2022-10-17 10:43:34 -0500

    select from usuario added succes

--- server/controllers/tasks.controllers.js
+++ server/controllers/tasks.controllers.js
@@ -3,7 +3,7 @@ import { pool } from "../db.js";
 export const getTasks = async (req, res) => {
     try {
         const [result] = await pool.query(
-            "SELECT * FROM usuario ORDER BY createAt ASC"
+            "SELECT * FROM usuario ORDER BY idUsuario ASC"
         );
         res.json(result);
     } catch (error) {
