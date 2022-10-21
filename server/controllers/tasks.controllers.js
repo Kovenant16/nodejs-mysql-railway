0d2067061f4ae6d2184ff61ed4cd5e1bd5304704
Irvin <reimer1693@gmail.com>
2022-10-21 11:27:09 -0500

    result 0 removed

--- server/controllers/tasks.controllers.js
+++ server/controllers/tasks.controllers.js
@@ -31,7 +31,7 @@ export const getTask = async (req, res) => {
         if (result.length === 0)
             return res.status(404).json({ message: "Task not found" });
 
-        res.json(result[0]);
+        res.json(result);
     } catch (error) {
         return res.status(500).json({ message: error.message });
     }
